05ccc2efdb0004ed240d719379cd0eb4efb75a7d
ww <wwong1801@gmail.com>
2025-11-23 01:27:16 +0800

    Fea/focus of week (#2)
    * Create focus-of-week.html
    
    * Rename focus-of-week.html to focus.html
    
    * Create focus.js
    
    * Update focus.js
    
    * Update focus.js
    
    * Update focus.html
    
    * Update focus.js
    
    * Update item.html
    
    * Update item.js

---
+++ JSON/focus.js
@@ -0,0 +1,12 @@
+db.collection("goals").where("Focus", "==", true)
+  .orderBy("createdAt", "desc")
+  .onSnapshot(snapshot => {
+    focusContainer.innerHTML = ""; // clear previous
+    snapshot.forEach(doc => {
+      const item = doc.data();
+      const div = document.createElement("div");
+      div.className = "bg-white p-3 rounded shadow";
+      div.innerHTML = `<strong>${item.goal} - ${item.type}:</strong> ${item.text}`;
+      focusContainer.appendChild(div);
+    });
+  });
