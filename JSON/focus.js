61e551f9a5955bacb013f615585adeb0965e1142
ww <wwong1801@gmail.com>
2025-11-23 01:19:39 +0800

    Update focus.js

--- JSON/focus.js
+++ JSON/focus.js
@@ -1,12 +1,12 @@
-const focusContainer = document.getElementById("focusContainer");
-
-db.collection("focus").orderBy("createdAt", "desc").onSnapshot(snapshot => {
-  focusContainer.innerHTML = ""; // clear previous
-  snapshot.forEach(doc => {
-    const item = doc.data();
-    const div = document.createElement("div");
-    div.className = "bg-white p-3 rounded shadow";
-    div.innerHTML = `<strong>${item.goal} - ${item.type}:</strong> ${item.text}`;
-    focusContainer.appendChild(div);
+db.collection("goals").where("Focus", "==", true)
+  .orderBy("createdAt", "desc")
+  .onSnapshot(snapshot => {
+    focusContainer.innerHTML = ""; // clear previous
+    snapshot.forEach(doc => {
+      const item = doc.data();
+      const div = document.createElement("div");
+      div.className = "bg-white p-3 rounded shadow";
+      div.innerHTML = `<strong>${item.goal} - ${item.type}:</strong> ${item.text}`;
+      focusContainer.appendChild(div);
+    });
   });
-});
